126383275064968f48e09df2055761d191b14b19
sahimasrivastava <srivastava.sahima@gmail.com>
2023-11-01 16:02:14 -0400

    Updated the folder name and refactored the directory structure

--- services/server/run.py
+++ services/server/run.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from models import db
 import time
-from routes import web_api
+from routes import server_api
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
     print(app.config)
-    app.register_blueprint(web_api)
+    app.register_blueprint(server_api)
     app.app_context().push()
     time.sleep(5)
     with app.app_context():
