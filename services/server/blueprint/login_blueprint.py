421c23e6eb09808ed4d418c874c8faffa173b7aa
RiyaParte <parteriya21@gmail.com>
2023-12-08 21:14:20 -0500

    Added login api

---
+++ services/server/blueprint/login_blueprint.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
+from controller.login_api_handler import LoginApiHandler as login_api_handler
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://patienttracker.swagger.io/v1/swagger.json'  # Our API url (can of course be a local resource)
+
+login_api = Blueprint('login_api', __name__, url_prefix='/api/login' )
+
+@login_api.route('/fetch', methods=['GET'])
+def fetch():
+    try:
+        all_cats = login_api_handler.fetch()
+    except Exception as e:
+        return handle_error(e, f"Error[{type(e)}]{str(e)}", "api/login/fetch")
+    return jsonify(all_cats), 200
+
+@login_api.route('/add', methods=['POST'])
+def add():
+    try:
+        ret_val = login_api_handler.add()
+    except Exception as e:
+        return handle_error(e, f"Error[{type(e)}]{str(e)}", "api/login/add")
+    return jsonify(ret_val), 200
+
+
+@login_api.app_errorhandler(Exception)
+def handle_error(error, messg="Unknown Error", api_name="login_api/", code=500):
+    response = {
+        'status': code,
+        'api': api_name,
+        'error': {
+            'type': error.__class__.__name__,
+            'message': messg
+        }
+    }
+    return jsonify(response), code
--- services/server/controller/user_api_handler.py
+++ services/server/controller/user_api_handler.py
@@ -1,9 +1,9 @@
 import os, sys
 sys.path.append(os.path.dirname(__file__))
 
-from flask import request, current_app
+from flask import request
 import database_handler as database
-from model.models import User, LoginDetails
+from model.models import *
 import json
 
 class UserApiHandler:
@@ -12,14 +12,13 @@ class UserApiHandler:
         return "SUCCESS"
 
     def fetch():
-        users = database.get_all(LoginDetails)
+        users = database.get_all(User)
         all_users = []
         for user in users:
             new_cat = {
-                "user_id": user.user_id,
-                "email_id": user.email_id,
-                "password": user.password,
-                "last_login_date": user.last_login_date
+                "name": user.name,
+                "email": user.email,
+                "active": user.active,
             }
 
             all_users.append(new_cat)
@@ -33,3 +32,25 @@ class UserApiHandler:
         active = data['active']
         database.add_instance(User, name=name, email=email, active=active)
         return 1
+
+
+
+
+# class patientApiHandler:
+#     def default():
+#         print("default")
+#         return "SUCCESS"
+    
+#     def fetch():
+#         patients = database.get_all(Patient)
+#         all_patients = []
+#         for user in patients:
+#             patient = {
+#                 "user_id": user.user_id,
+#                 "email_id": user.email_id,
+#                 "password": user.password,
+#                 "last_login_date": user.last_login_date
+#             }
+
+#             all_patients.append(patient)
+#         return all_patients
