7eb786885974c00e81af7967d1cf652f7c81d677
sahimasrivastava <srivastava.sahima@gmail.com>
2023-12-14 14:16:49 -0500

    updated error handling

--- services/server/blueprint/doctor_blueprint.py
+++ services/server/blueprint/doctor_blueprint.py
@@ -32,19 +32,50 @@ def get_available_time():
     try:
         user_id = int(request.args.get('user_id'))  # read into integer type
         ret_val = appointment_api_handler.get_available_time(user_id)
+        if ret_val['doctor_found_flag'] == 0:
+            return handle_error(Exception('Bad Request: Doctor does not exist.'),
+                                'Bad Request: Doctor does not exist.',
+                                'Bad Request: Doctor does not exist.',
+                                "api/doctor/get-available-time",
+                                400)
     except Exception as e:
-        return handle_error(e, f"Error[{type(e)}]{str(e)}", "api/doctor/get-available-time")
-    return jsonify(ret_val), 200
+        return handle_error(e,
+                            'Internal Server Error: Please try again.',
+                            f"Error[{type(e)}]{str(e)}",
+                            "api/doctor/get-available-time",
+                            500)
+    return jsonify(ret_val['available_time']), 200
 
 @doctor_api.route('/book-appointment', methods=['POST'])
 @cross_origin(origin='localhost')
 def book_appointment():
     try:
         ret_val = appointment_api_handler.book_appointment()
-        if ret_val == 0:
-            return jsonify({"status": 400, "message": "Appointment already booked"}), 400
+        if ret_val['doctor_found_flag'] == 0:
+            return handle_error(Exception('Bad Request: Doctor does not exist.'),
+                                'Bad Request: Doctor does not exist.',
+                                'Bad Request: Doctor does not exist.',
+                                "api/doctor/book-appointment",
+                                400)
+        elif ret_val['patient_found_flag'] == 0:
+            return handle_error(Exception('Bad Request: Patient does not exist.'),
+                                'Bad Request: Patient does not exist.',
+                                'Bad Request: Patient does not exist.',
+                                "api/doctor/book-appointment",
+                                400)
+        else:
+            if ret_val['ret_val'] == 0:
+                return handle_error(Exception('Bad Request: Appointment already booked.'),
+                                    'Bad Request: Appointment already booked.',
+                                    'Bad Request: Appointment already booked.',
+                                    "api/doctor/book-appointment",
+                                    400)
     except Exception as e:
-        return handle_error(e, f"Error[{type(e)}]{str(e)}", "api/doctor/book-appointment")
+        return handle_error(e,
+                            'Internal Server Error: Please try again.',
+                            f"Error[{type(e)}]{str(e)}",
+                            "api/doctor/book-appointment",
+                            500)
     return jsonify(ret_val), 200
 
 @doctor_api.route('/get-all-doctor', methods=['GET'])
@@ -62,13 +93,14 @@ def get_all_doctor():
 
 
 @doctor_api.app_errorhandler(Exception)
-def handle_error(error, messg="Unknown Error", api_name="doctor_api/", code=500):
+def handle_error(error, messg="Unknown Error", error_trace="Unknown Error", api_name="doctor_api/", code=500):
     response = {
         'status': code,
         'api': api_name,
         'error': {
             'type': error.__class__.__name__,
-            'message': messg
+            'trace': error_trace,
+            'message': messg,
         }
     }
     return jsonify(response), code
